ff10f7c7b6e2eeb68989a51958ca65c0419663bd
Atakanz <atakanzer@gmail.com>
2022-08-31 16:32:00 +0300

    Login page designed.

--- Telegram/src/Pages/LoginPage/loginPage.js
+++ Telegram/src/Pages/LoginPage/loginPage.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Alert, Text} from 'react-native';
+import Buttons from '../../Components/Buttons/Buttons';
+import {ThemeContext} from '../../utils/themeManager';
+import {UserContext} from '../../utils/userManager';
+import {LoginContext} from '../../utils/loginManager';
+import FormUnit from '../../Components/textınputs/formUnit';
+import styles from './loginPage.style';
 
 const LoginPage = () => {
+  const {user} = React.useContext(UserContext);
+  const {theme} = React.useContext(ThemeContext);
+  const {setUser} = React.useContext(UserContext);
+  const [userName, setUserName] = React.useState(null);
+  const [userPassword, setUserPassword] = React.useState(null);
+  const {setIsLoggedIn} = React.useContext(LoginContext);
+
+  const collectUser = () => {
+    if (!userName || !userPassword) {
+      Alert.alert('Telegram', 'Bilgiler boş bırakılamaz');
+      return;
+    }
+    setUser([userName, userPassword]);
+    setIsLoggedIn(true);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Hello loginPage!</Text>
+    <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+      <FormUnit labelName="İsim" holder="User Name" task={setUserName} />
+      <FormUnit
+        labelName="Şifre giriniz"
+        holder="Password"
+        task={setUserPassword}
+      />
+      <Buttons name="Login" screenName="Rehber" task={collectUser} />
     </SafeAreaView>
   );
 };
---
+++ Telegram/src/Pages/LoginPage/loginPage.style.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerDark: {
+    backgroundColor: '#212121',
+  },
+  containerLight: {
+    backgroundColor: '#fff',
+  },
+});
