570c1c02ebd86b05608af2a233041737b94dc5ee
Atakanz <atakanzer@gmail.com>
2022-08-31 16:42:31 +0300

    Buttons onPress prop edited.

--- Telegram/src/Components/Buttons/Buttons.js
+++ Telegram/src/Components/Buttons/Buttons.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import {TouchableOpacity, Text} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
 import {ThemeContext} from '../../utils/themeManager';
 import styles from './Buttons.style';
 
-const Buttons = ({name, task, screenName}) => {
+const Buttons = ({name, task}) => {
   const {theme} = React.useContext(ThemeContext);
-  const navigation = useNavigation();
-  const sendRoute = () => navigation.navigate(screenName);
   return (
     <TouchableOpacity
       style={[styles.button, styles[`button${theme}`]]}
-      onPress={() => {
-        task();
-        sendRoute();
-      }}>
+      onPress={task}>
       <Text style={[styles.text, styles[`text${theme}`]]}>{name}</Text>
     </TouchableOpacity>
   );
