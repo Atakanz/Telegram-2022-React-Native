6acb3ab4e82c02fe89c21bfe9135b75a7c780fd1
Atakanz <atakanzer@gmail.com>
2022-08-31 16:26:03 +0300

    User and login providers added.

--- Telegram/src/App.js
+++ Telegram/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {LoginStack} from './navigation/loginStack';
-import {SettingStack} from './navigation/settingStack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LoginStack } from './navigation/loginStack';
+import { SettingStack } from './navigation/settingStack';
 import MessageList from './Pages/MessageListPage/messageListPage';
-import {ThemeProvider} from './utils/themeManager';
+import { ThemeProvider } from './utils/themeManager';
+import { UserProvider } from './utils/userManager';
+import { LoginProvider } from './utils/loginManager';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <ThemeProvider>
-        <Tab.Navigator screenOptions={{headerShown: false}}>
-          <Tab.Screen name="LoginTab" component={LoginStack} />
-          <Tab.Screen name="MessageTab" component={MessageList} />
-          <Tab.Screen name="SettingsTab" component={SettingStack} />
-        </Tab.Navigator>
+        <LoginProvider>
+          <UserProvider>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+              <Tab.Screen name="LoginTab" component={LoginStack} />
+              <Tab.Screen name="MessageTab" component={MessageList} />
+              <Tab.Screen name="SettingsTab" component={SettingStack} />
+            </Tab.Navigator>
+          </UserProvider>
+        </LoginProvider>
       </ThemeProvider>
     </NavigationContainer>
   );
