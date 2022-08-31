47332d51f37a8172da4beb52c3964a791569cd61
Atakanz <atakanzer@gmail.com>
2022-08-31 17:34:40 +0300

    Loginroute is conditioned for logged-in status.

--- Telegram/src/navigation/loginStack.js
+++ Telegram/src/navigation/loginStack.js
@@ -3,12 +3,14 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from '../Pages/LoginPage/loginPage';
 import Contacts from '../Pages/ContactsPage/contactsPage';
 import MessageArea from '../Pages/MessagePage/messagePage';
+import LoginSecond from '../Pages/LoginPageSecond/loginPageSecond';
 import {ThemeContext} from '../utils/themeManager';
-
+import {LoginContext} from '../utils/loginManager';
 const Stack = createNativeStackNavigator();
 
 export const LoginStack = () => {
   const {theme} = React.useContext(ThemeContext);
+  const {isLoggedIn} = React.useContext(LoginContext);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -17,7 +19,10 @@ export const LoginStack = () => {
         },
         headerTintColor: theme === 'Dark' ? '#fff' : '#212121',
       }}>
-      <Stack.Screen name="Giriş" component={LoginPage} />
+      <Stack.Screen
+        name="Giriş"
+        component={isLoggedIn ? LoginSecond : LoginPage}
+      />
       <Stack.Screen name="Rehber" component={Contacts} />
       <Stack.Screen name="MessagePage" component={MessageArea} />
     </Stack.Navigator>
