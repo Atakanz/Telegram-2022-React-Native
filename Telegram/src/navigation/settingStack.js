9007f9791e7083d2c1d85359a534595a0016a36a
Atakanz <atakanzer@gmail.com>
2022-08-31 14:24:20 +0300

    Header dark-light style added.

--- Telegram/src/navigation/settingStack.js
+++ Telegram/src/navigation/settingStack.js
@@ -3,14 +3,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import editingPage from '../Pages/EditingPage/editingPage';
 import themePage from '../Pages/ThemePage/themePage';
 import settingsPage from '../Pages/SettingsPage/settingsPage';
+import {ThemeContext} from '../utils/themeManager';
 
 const Stack = createNativeStackNavigator();
 
 export const SettingStack = () => {
+  const {theme} = React.useContext(ThemeContext);
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme === 'Dark' ? '#212121' : '#fff',
+        },
+        headerTintColor: theme === 'Dark' ? '#fff' : '#212121',
+      }}>
       <Stack.Screen name="Settings" component={settingsPage} />
-      <Stack.Screen name="themeSettings" component={themePage} />
+      <Stack.Screen name="Tema" component={themePage} />
       <Stack.Screen name="editingSettings" component={editingPage} />
     </Stack.Navigator>
   );
