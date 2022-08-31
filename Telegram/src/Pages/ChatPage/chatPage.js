a24ef4870cf7a3afa5388bca57b60672cd19589e
Atakanz <atakanzer@gmail.com>
2022-09-01 02:19:50 +0300

    Top-bar parameters added.

--- Telegram/src/Pages/ChatPage/chatPage.js
+++ Telegram/src/Pages/ChatPage/chatPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import TopBar from '../../Components/topBar/topBar';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const phone = <Icon name="phone" size={30} color="black" />;
+const dots = <Icon name="dots-vertical" size={30} color="black" />;
 
 const ChatPage = ({route, navigation}) => {
   const {item} = route.params;
   return (
     <SafeAreaView>
-      <Text>{item.name}</Text>
+      <TopBar src={item.image} name={item.name} icon1={phone} icon2={dots} />
     </SafeAreaView>
   );
 };
