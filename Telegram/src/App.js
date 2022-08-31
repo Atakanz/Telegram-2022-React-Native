8182eb6f3090a92e2b186efcdcf2c15631563bf7
Atakanz <atakanzer@gmail.com>
2022-08-31 16:51:28 +0300

    TabBar set to hidden in Login Page.

--- Telegram/src/App.js
+++ Telegram/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { LoginStack } from './navigation/loginStack';
-import { SettingStack } from './navigation/settingStack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {getFocusedRouteNameFromRoute} from '@react-navigation/native';
+import {LoginStack} from './navigation/loginStack';
+import {SettingStack} from './navigation/settingStack';
 import MessageList from './Pages/MessageListPage/messageListPage';
-import { ThemeProvider } from './utils/themeManager';
-import { UserProvider } from './utils/userManager';
-import { LoginProvider } from './utils/loginManager';
+import {ThemeProvider} from './utils/themeManager';
+import {UserProvider} from './utils/userManager';
+import {LoginProvider} from './utils/loginManager';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
@@ -15,8 +16,14 @@ const App = () => {
       <ThemeProvider>
         <LoginProvider>
           <UserProvider>
-            <Tab.Navigator screenOptions={{ headerShown: false }}>
-              <Tab.Screen name="LoginTab" component={LoginStack} />
+            <Tab.Navigator screenOptions={{headerShown: false}}>
+              <Tab.Screen
+                name="LoginTab"
+                component={LoginStack}
+                options={({route}) => ({
+                  tabBarStyle: {display: getRouteName(route)},
+                })}
+              />
               <Tab.Screen name="MessageTab" component={MessageList} />
               <Tab.Screen name="SettingsTab" component={SettingStack} />
             </Tab.Navigator>
@@ -25,6 +32,14 @@ const App = () => {
       </ThemeProvider>
     </NavigationContainer>
   );
+};
+
+const getRouteName = route => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'FirstPageDefault';
+  if (routeName?.includes('Giriş') || routeName?.includes('FirstPageDefault')) {
+    return 'none';
+  }
+  return 'flex';
 };
 
 export default App;
