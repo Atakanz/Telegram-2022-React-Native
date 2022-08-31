b56c34ad4d1d9ea4da90139693dbc554aef8c6ef
Atakanz <atakanzer@gmail.com>
2022-08-31 16:31:21 +0300

    Screen options added to loginstack.

--- Telegram/src/navigation/loginStack.js
+++ Telegram/src/navigation/loginStack.js
@@ -3,14 +3,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from '../Pages/LoginPage/loginPage';
 import Contacts from '../Pages/ContactsPage/contactsPage';
 import MessageArea from '../Pages/MessagePage/messagePage';
+import {ThemeContext} from '../utils/themeManager';
 
 const Stack = createNativeStackNavigator();
 
 export const LoginStack = () => {
+  const {theme} = React.useContext(ThemeContext);
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginPage} />
-      <Stack.Screen name="Contacts" component={Contacts} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme === 'Dark' ? '#212121' : '#fff',
+        },
+        headerTintColor: theme === 'Dark' ? '#fff' : '#212121',
+      }}>
+      <Stack.Screen name="Giriş" component={LoginPage} />
+      <Stack.Screen name="Rehber" component={Contacts} />
       <Stack.Screen name="MessagePage" component={MessageArea} />
     </Stack.Navigator>
   );
