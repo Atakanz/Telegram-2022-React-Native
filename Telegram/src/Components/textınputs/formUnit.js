0454336e24f3089d0540cd3f2dbfec45f27ae436
Atakanz <atakanzer@gmail.com>
2022-08-31 16:30:28 +0300

    Form unit-text inputs added.

---
+++ Telegram/src/Components/textınputs/formUnit.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {TextInput} from 'react-native';
+import {ThemeContext} from '../../utils/themeManager';
+import styles from './formUnits.style';
+
+const FormUnit = props => {
+  const {theme} = React.useContext(ThemeContext);
+  return (
+    <TextInput
+      style={[styles.formUnit, [styles[`formUnit${theme}`]]]}
+      label={props.labelName}
+      placeholder={props.holder}
+      placeholderTextColor={theme === 'Light' ? '#212121' : '#fff'}
+      onChangeText={props.task}
+    />
+  );
+};
+
+export default FormUnit;
---
+++ Telegram/src/Components/textınputs/formUnits.style.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  formUnit: {
+    borderRadius: 20,
+    borderWidth: 2.5,
+    width: '70%',
+    borderColor: '#0088CC',
+    marginVertical: 10,
+  },
+  formUnitDark: {
+    backgroundColor: '#212121',
+  },
+  formUnitLight: {
+    backgroundColor: '#fff',
+  },
+});
