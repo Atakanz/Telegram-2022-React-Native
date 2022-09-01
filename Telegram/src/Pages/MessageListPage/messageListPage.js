a12c6ddd4c9f4c166f77563375fa3f270e8730d5
Atakanz <atakanzer@gmail.com>
2022-09-02 00:13:00 +0300

    Message list page added.

--- Telegram/src/Pages/MessageListPage/messageListPage.js
+++ Telegram/src/Pages/MessageListPage/messageListPage.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, FlatList, View, Text} from 'react-native';
+import {ReceiverContext} from '../../utils/receiverManager';
+import {MessageContext} from '../../utils/messageBoxManager';
+import {ThemeContext} from '../../utils/themeManager';
+import TopBar from '../../Components/topBar/topBar';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MessageListCard from '../../Components/MessageListCards/MessageListCards';
+import styles from './messageListPage.style';
 
 const MessageList = () => {
+  const {receiver} = React.useContext(ReceiverContext);
+  const {message} = React.useContext(MessageContext);
+  const {theme} = React.useContext(ThemeContext);
+  const {lastMessageItem} = React.useContext(MessageContext);
+  const colorSelect = theme === 'Dark' ? 'white' : '#212121';
+  const magnify = <Icon name="magnify" size={25} color={colorSelect} />;
   return (
-    <SafeAreaView>
-      <Text>Hello messageListPage!</Text>
+    <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+      <TopBar name="Telegram" icon2={magnify} />
+      <Text>{receiver.id}</Text>
+      <View style={styles.container}>
+        {/* <View style={styles.container}></View> */}
+        <FlatList
+          data={receiver}
+          renderItem={({item}) => (
+            <MessageListCard
+              name={item.name}
+              link={item.image}
+              lastMessage={lastMessageItem}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </SafeAreaView>
   );
 };
---
+++ Telegram/src/Pages/MessageListPage/messageListPage.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  containerDark: {
+    backgroundColor: '#212121',
+  },
+  containerLight: {
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+  },
+});
